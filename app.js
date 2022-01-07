91bc1cb1f11645548472a1dc8fec140fc0d4ef27
smshack <5432tat@naver.com>
2022-01-08 00:12:11 +0900

    요청과 응답

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express from "express";
+import cookieParser from 'cookie-parser'
+
 const app = express();
 const port = 8080
 
-app.get('/',(req,res,next)=>{
-    console.log('get')
-    res.send('hello world')
+app.use(cookieParser())
+
+app.get('/sky/:id',(req,res,next)=>{
+    const reqs ={
+        path:req.path,
+        headers:req.headers,
+        params:req.params,
+        query:req.query,
+        cookie:req.cookies
+    }
+    res.setHeader('key','value')
+    res.status(200).send(reqs)
 })
 
 
