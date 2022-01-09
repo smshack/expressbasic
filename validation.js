23a4096114dc40978ce4e16e133bd8d78cff4f06
smshack <5432tat@naver.com>
2022-01-09 19:59:49 +0900

    유효성 검사 수정

--- validation.js
+++ validation.js
@@ -9,7 +9,7 @@ const validate = (req, res, next) => {
   if (errors.isEmpty()) {
     return next();
   }
-  return res.status(400).json({ message: errors.array()[0].msg });
+  return res.status(400).json({ message: errors.array() });
 };
 
 app.post(
